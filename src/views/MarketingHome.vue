2ef3857107d8ec6c255a3173ead6e2b8a63e66c5
HyeonJeong <yoonhj97@naver.com>
2020-09-03 17:34:36 +0900

    0903 Marketing 작업중

--- src/views/MarketingHome.vue
+++ src/views/MarketingHome.vue
@@ -5,9 +5,15 @@
       <div class="section1">
         <p class="title">MARKETING</p>
         <p class="desc">Template by w3.css</p>
-        <div>
-
+        <div class="indicator">
+          <img :src="IDimages[0].src" />
+          <div class="footer">
+            <!--            <i class="el-icon-caret-left"></i>-->
+          </div>
         </div>
+      </div>
+      <div class="section2">
+
       </div>
     </div>
   </div>
@@ -26,12 +32,20 @@
     id: 4, title: 'Contact',
   },]
 
+  const IDimages = [{
+    id: 1, src: require('../assets/images/Marketing/indicator1.jpg'),
+  },{
+    id: 2, src: require('../assets/images/Marketing/indicator2.jpg'),
+  },{
+    id: 3, src: require('../assets/images/Marketing/indicator3.jpg'),
+  },]
+
   export default {
     name: 'Home',
     components: {HomeHeader,},
     data() {
       return {
-        headerMenus,
+        headerMenus, IDimages,
       }
     }
   }
